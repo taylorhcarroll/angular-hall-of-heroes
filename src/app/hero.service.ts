f47b5e7ab4140930ea078633f1bbac2ae4164547
Taylor <taylorhcarroll1@gmail.com>
2020-07-26 16:45:49 -0500

    a message service now

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     //fetch to get heroes from api,
     //based on endpoint being hit, backend
     //will return JSONo object of array of Hero objects
     //will set array = to HEROES
+
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes')
     return of(HEROES);
   }
 }
